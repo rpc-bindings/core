af29063b0fd48ed52c0f002c98c64146ff27eaf7
David Serfozo <david@serfozo.net>
2018-02-07 21:02:08 +0100

    Added readonly binding repository

---
+++ src/DSerfozo.RpcBindings/Extensions/BindingRepositoryExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using DSerfozo.RpcBindings.Contract;
+using DSerfozo.RpcBindings.Model;
+
+namespace DSerfozo.RpcBindings.Extensions
+{
+    public static class BindingRepositoryExtensions
+    {
+        private class ReadOnlyBindingRepositry : IBindingRepository
+        {
+            private readonly IBindingRepository wrapped;
+
+            public IReadOnlyDictionary<int, ObjectDescriptor> Objects => wrapped.Objects;
+
+            public ReadOnlyBindingRepositry(IBindingRepository wrapped)
+            {
+                this.wrapped = wrapped;
+            }
+
+            public void AddBinding<TObject>(string key, TObject obj)
+            {
+                throw new InvalidOperationException("This instance is read-only.");
+            }
+
+            public void AddBinding(string key, object obj)
+            {
+                throw new InvalidOperationException("This instance is read-only.");
+            }
+
+            public void Dispose()
+            {
+                throw new InvalidOperationException("Readonly wrapper is not disposable");
+            }
+        }
+
+        public static IBindingRepository AsReadOnly(this IBindingRepository @this)
+        {
+            return new ReadOnlyBindingRepositry(@this);
+        }
+    }
+}
---
+++ tests/DSerfozo.RpcBindings.Tests/Extensions/BindingRepositoryExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using System;
+using DSerfozo.RpcBindings.Contract;
+using DSerfozo.RpcBindings.Extensions;
+using Xunit;
+
+namespace DSerfozo.RpcBindings.Tests.Extensions
+{
+    public class BindingRepositoryExtensionsTests
+    {
+        [Fact]
+        public void BindingRepositoryReadOnly()
+        {
+            var repository = Mock.Of<IBindingRepository>();
+            var readOnly = repository.AsReadOnly();
+
+            Assert.Throws<InvalidOperationException>(() => readOnly.AddBinding("key", new object()));
+        }
+
+        [Fact]
+        public void NotDisposable()
+        {
+            var repository = Mock.Of<IBindingRepository>();
+            var readOnly = repository.AsReadOnly();
+
+            Assert.Throws<InvalidOperationException>(() => readOnly.Dispose());
+        }
+    }
+}
